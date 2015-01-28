17743f2cfd05486b5b06a888fbfe93b9fd95ec7c
Calvin Nisbet <cnisb044@uottawa.ca>
2015-01-28 12:37:20 -0800

    first commit

---
+++ Every.do/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Every.do
+//
+//  Created by Calvin Nisbet on 2015-01-28.
+//  Copyright (c) 2015 Calvin Nisbet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
